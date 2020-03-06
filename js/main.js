ca1da62e5b8f80b4e71ebcb038edbb8bfb7f44d6
Troy Chryssos <troychryssos@gmail.com>
2020-03-05 22:53:45 -0500

    some organization

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-import pitcher1 from '../static/Pitcher1.svg'
-const pitcher = document.getElementById('pitcher')
+import pitcherSvg1 from '../static/Pitcher1.svg'
+import pitcherSvg2 from '../static/Pitcher2.svg'
+import pitcherSvg3 from '../static/Pitcher3.svg'
 
-pitcher.src = pitcher1
+// CONSTANTS
+const pitcher = document.getElementById('pitcher')
+const setSvg1 = () => pitcher.src = pitcherSvg1
+const setSvg2 = () => pitcher.src = pitcherSvg2
+const setSvg3 = () => pitcher.src = pitcherSvg3
+
+// LISTENERS
+document.addEventListener('click', setSvg2)
+
+// START
+pitcher.src = pitcherSvg1
 
