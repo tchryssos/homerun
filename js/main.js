7af0128edb02eb23f95102ff262a146c28fa4ce0
Troy Chryssos <troychryssos@gmail.com>
2020-03-06 22:53:16 -0500

    async timeouts for animation

--- js/main.js
+++ js/main.js
@@ -2,14 +2,37 @@ import pitcherSvg1 from '../static/Pitcher1.svg'
 import pitcherSvg2 from '../static/Pitcher2.svg'
 import pitcherSvg3 from '../static/Pitcher3.svg'
 
+// TIMING
+let animating = false
+
 // CONSTANTS
 const pitcher = document.getElementById('pitcher')
+const batterBox = document.getElementById('batterBox')
 const setSvg1 = () => pitcher.src = pitcherSvg1
 const setSvg2 = () => pitcher.src = pitcherSvg2
 const setSvg3 = () => pitcher.src = pitcherSvg3
 
+const timeout = (func, ms) => (
+	new Promise((resolve) => (
+		setTimeout(() => {
+			func()
+			resolve()
+		}, ms)
+	))
+)
+
+const pitch = async () => {
+	if (animating === false) {
+		animating = true
+		await timeout(setSvg2, 1500)
+		await timeout(setSvg3, 1500)
+		await timeout(setSvg1, 1500)
+		animating = false
+	}
+}
+
 // LISTENERS
-document.addEventListener('click', setSvg2)
+batterBox.addEventListener('click', pitch)
 
 // START
 pitcher.src = pitcherSvg1
