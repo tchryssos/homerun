3b935234c63535cf4cd9737273c606c3ada283ad
Troy Chryssos <troychryssos@gmail.com>
2020-03-06 23:09:54 -0500

    now thats a real pitch animation

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@ import pitcherSvg3 from '../static/Pitcher3.svg'
 let animating = false
 
 // CONSTANTS
+const pitchSpeed = 4000
+const frameSpeed = 1500
 const pitcher = document.getElementById('pitcher')
 const batterBox = document.getElementById('batterBox')
+const ball = document.getElementById('ball')
 const setSvg1 = () => pitcher.src = pitcherSvg1
 const setSvg2 = () => pitcher.src = pitcherSvg2
 const setSvg3 = () => pitcher.src = pitcherSvg3
@@ -21,13 +24,23 @@ const timeout = (func, ms) => (
 	))
 )
 
+const ballThrow = () => {
+	ball.style.display = 'block'
+	ball.style.animationName = 'ballThrow'
+	timeout(() => ball.style.display = 'none', 4000)
+}
+
 const pitch = async () => {
 	if (animating === false) {
 		animating = true
-		await timeout(setSvg2, 1500)
-		await timeout(setSvg3, 1500)
-		await timeout(setSvg1, 1500)
+		batterBox.style.display = 'none'
+		await timeout(setSvg2, frameSpeed)
+		await timeout(setSvg3, frameSpeed)
+		ballThrow()
+		await timeout(setSvg1, frameSpeed)
+		await timeout(() => {}, pitchSpeed - frameSpeed)
 		animating = false
+		batterBox.style.display = 'block'
 	}
 }
 
