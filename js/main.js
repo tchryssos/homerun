01012d38c927bc60c9f8c248728e58483843831e
Troy Chryssos <troychryssos@gmail.com>
2020-03-15 13:30:20 -0400

    you can hit the ball, but it just kind of glides up

--- js/main.js
+++ js/main.js
@@ -3,17 +3,28 @@ import pitcherSvg2 from '../static/Pitcher2.svg'
 import pitcherSvg3 from '../static/Pitcher3.svg'
 
 // ANIMATION & TIMING
-let animating = false
-let hit = false
-
-let translateY = 0
-const yTarget = 20
-let scale = 1
-const scaleTarget = 80
-
-// CONSTANTS
+let isAnimating = false
+let isHit = false
+let pitchTime
+const fps = 60 // @TODO Find a way to target actual refresh rate
 const pitchSpeed = 3000
 const frameSpeed = 1500
+const hitSpeed = 6000
+
+let translateY = 0
+let hitY
+const pitchYTarget = 20
+const hitYTarget = -40
+
+let translateX = 0
+const hitXTarget = 90
+
+let scale = 1
+let hitScale
+const pitchScaleTarget = 80
+const hitScaleTarget = 0.5
+
+// ELEMENTS
 const pitcher = document.getElementById('pitcher')
 const batterBox = document.getElementById('batterBox')
 const ball = document.getElementById('ball')
@@ -21,6 +32,7 @@ const setSvg1 = () => pitcher.src = pitcherSvg1
 const setSvg2 = () => pitcher.src = pitcherSvg2
 const setSvg3 = () => pitcher.src = pitcherSvg3
 
+// ANIMATION FUNCTIONS
 const timeout = (func, ms) => (
 	new Promise((resolve) => (
 		setTimeout(() => {
@@ -30,18 +42,41 @@ const timeout = (func, ms) => (
 	))
 )
 
-// @TODO Find a way to target actual refresh rate
-const targetMod = target => (target / (pitchSpeed / 1000) / 60)
+const endPitchCycle = async () => {
+	await timeout(
+		() => ball.style.display = 'none', 
+		pitchSpeed
+	)
+	isAnimating = false
+	batterBox.style.display = 'block'
+}
+
+const targetMod = (target, speed) => (target / (pitchSpeed / 1000) / fps)
+
+const hit = () => {
+	requestAnimationFrame(() => {
+		translateY += targetMod(hitYTarget - hitY, hitSpeed)
+		ball.style.transform = `scale(${scale}) translateY(${translateY}px)`
+		hit()
+		// translateX = translateX + (hitSpeed / fps)
+		// scale = scale - (hitSpeed / fps)
+	})
+}
 
 const pitch = () => (
 	requestAnimationFrame(() => {
-		if (!hit) {
-			scale += targetMod(scaleTarget)
-			translateY += targetMod(yTarget)
+		if (!isHit) {
+			if (Date.now() - pitchTime >= pitchSpeed) {
+				ball.style.display = 'none'
+				return
+			}
+			scale += targetMod(pitchScaleTarget, pitchSpeed)
+			console.log(scale)
+			translateY += targetMod(pitchYTarget, pitchSpeed)
 			ball.style.transform = `scale(${scale}) translateY(${translateY}px)`
 			pitch()
 		} else {
-			ball.style.display = 'none'
+			hit()
 		}
 	})
 )
@@ -49,28 +84,30 @@ const pitch = () => (
 const ballVisible = () => {
 	ball.style.display = 'block'
 	ball.style.cursor = 'pointer'
+	pitchTime = Date.now()
 	pitch()
 }
 
 const runPitchAnimation = async () => {
-	if (animating === false) {
-		hit = false
-		animating = true
+	if (isAnimating === false) {
+		isHit = false
+		isAnimating = true
 		batterBox.style.display = 'none'
 		await timeout(setSvg2, frameSpeed)
 		await timeout(setSvg3, frameSpeed)
 		ballVisible()
 		setTimeout(setSvg1, frameSpeed)
-		await timeout(
-			() => ball.style.display = 'none', 
-			pitchSpeed
-		)
-		animating = false
-		batterBox.style.display = 'block'
+		if (!hit) {
+			endPitchCycle()
+		}
 	}
 }
 
-const homerun = () => hit = true
+const homerun = () => {
+	isHit = true
+	hitY = translateY
+	hitScale = scale
+}
 
 // LISTENERS
 batterBox.addEventListener('click', runPitchAnimation)
